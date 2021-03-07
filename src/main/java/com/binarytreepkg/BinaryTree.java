07d18ec36d22727845416e45c7f04e52ba8ba1a8
JasKaur96 <jaspreetflora7@gmail.com>
2021-03-07 13:12:01 +0530

    Added: Create Binary Tree

--- src/main/java/com/binarytreepkg/BinaryTree.java
+++ src/main/java/com/binarytreepkg/BinaryTree.java
@@ -1,7 +1,7 @@
 package com.binarytreepkg;
 
 public class BinaryTree <E extends Comparable<E>>{
-    private MyBinaryNode<E> root;
+    private BinaryNode<E> root;
 
     public BinaryTree(){
         this.root = null;
@@ -10,17 +10,16 @@ public class BinaryTree <E extends Comparable<E>>{
     public void add(E data) {
         this.root = this.addNode(root, data);
     }
-
-    private MyBinaryNode<E> addNode(MyBinaryNode<E> current, E data) {
+    private BinaryNode<E> addNode(BinaryNode<E> current, E data) {
         // When the node is null create new node
         if(current == null) {
-            return new MyBinaryNode<E>(data);
+            return new BinaryNode<E>(data);
         }
-        //If element is less than current node key add to left
+        // if data is less than current node key add to left
         else if(current.key.compareTo(data)>0) {
             current.leftNode = addNode(current.leftNode, data);
         }
-        // If element is greater than current node key add to right
+        // if data is more than current node key add to right
         else {
             current.rightNode = addNode(current.rightNode, data);
         }
